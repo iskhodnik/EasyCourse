1792c30b454edf93e391f4498af4a31ebaac801e
Nikolay <bogdanov.n.g@gmail.com>
2019-08-01 17:32:59 +0300

    Создан проект 2-й домашки.

---
+++ BookstoreHierarchy/BookstoreHierarchy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookstoreHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
