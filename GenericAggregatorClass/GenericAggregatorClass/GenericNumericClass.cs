cf2129151978846a2553e5bcc1e1fad1bb27dfb6
Nik <bogdanov.n.g@gmail.com>
2019-09-16 02:19:36 +0300

    Added project "GenericAggregatorClass".

---
+++ GenericAggregatorClass/GenericAggregatorClass/GenericNumericClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericAggregatorClass
+{
+    public class GenericNumericClass<T>
+    {
+        private List<T> listObjects;
+
+        public GenericNumericClass(List<T> listObjects)
+        {
+            if (TypeCheck(listObjects))
+            {
+                this.listObjects = listObjects ?? throw new NullReferenceException();
+            }
+        }
+
+        public T Min()
+        {
+            return listObjects.Min();
+        }
+
+        public T Max()
+        {
+            return listObjects.Max();
+        }
+
+        private bool TypeCheck(List<T> listObjects)
+        {
+            switch (Type.GetTypeCode(typeof(T)))
+            {
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return true;
+                default:
+                    throw new ArgumentException($"{typeof(T)} - Неподдерживаемый тип данных");
+            }
+        }
+
+        public double Average()
+        {
+            return listObjects.Select(s => double.Parse(s.ToString())).Average();
+        }
+    }
+}
---
+++ GenericAggregatorClass/GenericAggregatorClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericAggregatorClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new GenericNumericClass<double>(new List<double>() { 1.5, 3.5, 5.5, 7.5, 9.5 });
+
+            Console.WriteLine(myList.Max());
+            Console.WriteLine(myList.Min());
+            Console.WriteLine(myList.Average());
+            Console.ReadKey();
+        }
+    }
+}
